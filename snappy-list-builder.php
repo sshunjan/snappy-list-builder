d0c50b50e75d79b04d80ec37cc0bca5467df8836
Unknown <simranjeet.hunjan@aceproject.space>
2018-01-17 10:13:09 -0600

    Updated some plugin info

--- snappy-list-builder.php
+++ snappy-list-builder.php
@@ -2,11 +2,11 @@
  include('functions.php');
 /*
 Plugin Name: Snappy List Builder
-Plugin URI: http://wordpressplugincourse.com/plugins/snappy-list-builder
+Plugin URI: #
 Description: The ultimate email list building plugin for WordPress. Capture new subscribers. Reward subscribers with a custom download upon opt-in. Build unlimited lists. Import and export subscribers easily with .csv
 Version: 1.0
-Author: Joel Funk @ Code College
-Author URI: http://joelfunk.codecollege.ca
+Author: Simranjeet Singh Hunjan
+Author URI: #
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: snappy-list-builder
